ebdced6df3b33a3ce971e184cf44b4035c1d9dce
Aaditya jadeja <shieldshadow2004@gmail.com>
2026-08-12 18:05:15 +0530

    completed advance-js

---
+++ advjs/impexp/math.js
@@ -0,0 +1,21 @@
+// export function add(a,b){
+//     return a+b;
+// }
+
+// export function subtract(a, b) {
+//     return a - b;
+// }
+
+function add(a,b){
+    return a+b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+export {add,multiply,subtract};
