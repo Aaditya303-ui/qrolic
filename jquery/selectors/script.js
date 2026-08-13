d92977566c4ae0ed00283e21bd3cadb5584dc75c
Aaditya jadeja <shieldshadow2004@gmail.com>
2026-08-13 11:01:31 +0530

    Use advanced selectors and attribute filters

---
+++ jquery/selectors/script.js
@@ -0,0 +1,36 @@
+// $(document).ready(function () {
+//   $("#btn").click(function () {
+//     $("p").hide();
+//   });
+//   $("#head").click(function () {
+//     $(".head1").hide();
+//   });
+// });
+
+
+// To hide all elements
+
+// $(document).ready(function(){
+//     $(".snap").click(function(){
+//         $("*").hide();
+//     });
+// });
+
+// Using this 
+// $(document).ready(function(){
+//     $('.me').click(function(){
+//         $(this).hide();
+//     });
+// });
+
+// $(document).ready(function(){
+//     $(".btn").click(function(){
+//         $("p:first").hide();
+//     });
+// });
+
+// $(document).ready(function(){
+//     $(".btn").click(function(){
+//         $("p:eq(0)").hide();
+//     });
+// });
