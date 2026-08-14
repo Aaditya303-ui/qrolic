7c8e0366a713283dfe2a507ca02a4efeeb69966e
Aaditya jadeja <shieldshadow2004@gmail.com>
2026-08-14 14:45:11 +0530

    learn Add/remove/replace content

---
+++ jquery/getset/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+
+//         console.log($("#home").data("author-name"));
+//         console.log($("#home").data("year"));
+
+//         $("#btn").click(function(){
+//             alert($("img").attr("data-villian"));
+//         });
+
+//         $("#change").click(function(){
+//             $("img").attr("data-villian","Galactus");
+//             alert($("img").attr("data-villian"));
+//         });
+
+//         console.log($("#home").html());
+//         console.log($("#home").text());
+//     });
