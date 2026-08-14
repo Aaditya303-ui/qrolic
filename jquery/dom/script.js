9216e9f656b79ffe01c1138fdd697fbdb8918ad3
Aaditya jadeja <shieldshadow2004@gmail.com>
2026-08-14 12:21:48 +0530

    DOM traversal using .parent(), .children(), .siblings()

---
+++ jquery/dom/script.js
@@ -0,0 +1,34 @@
+
+    // $(".child-two").parent().css("border", "2px solid red");
+    //  $(".parent").parents().css("background-color","yellow");
+    // $(".parent").parent().css("background-color", "yellow");
+    //$(".child-two").parentsUntil().css("border", "2px solid black");
+    //$(".child-two").parentsUntil(".god").css("background-color", "red");
+    //$(".god").children().css("background-color","yellow");
+    //$(".great-grand-parent").find("li").css("background-color", "yellow");
+    //$(".god").find(".target").css("background-color","green");
+    //$(document).ready(function () {
+    //$("#btn").click(function () {
+    //  $(".parent")
+    //    .parentsUntil(".great-grand-parent")
+    //    .css("border", "2px solid red");
+    //});
+
+    //$("#btn").click(function(){
+    //$(".child-two").parent().css("background-color","aqua");
+    //$(".grand-parent").children().css("background-color","yellow");
+    //})
+    //});
+
+    //$("h2").siblings().css("background-color","yellow");
+    //$("h2").next().css("background-color","yellow");
+    //$("h2").nextAll().css("background-color","yellow");
+    //$("p").nextUntil("h3").css("background-color","yellow");
+    //$("p").prev("h3").css("background-color","yellow");
+    //$("p").prevAll().css("background-color","yellow");
+    //$("p").prevUntil("span").css("background-color","yellow");
+    //$("div").first().css("background-color","yellow");
+    //$("div").last().css("background-color","aqua");
+    //$("div").eq(1).css("background-color","orange");
+    //$("div").filter(".intro").css("background-color","orange");
+    //$("div").not(".intro").css("background-color","pink");
