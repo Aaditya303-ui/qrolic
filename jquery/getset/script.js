82b434bfacc35b181f268d111774da3bed7ed82b
Aaditya jadeja <shieldshadow2004@gmail.com>
2026-08-17 10:58:06 +0530

    starting with add class

--- jquery/getset/script.js
+++ jquery/getset/script.js
@@ -1,17 +1,45 @@
 // $(document).ready(function(){
-
-//         console.log($("#home").data("author-name"));
-//         console.log($("#home").data("year"));
-
 //         $("#btn").click(function(){
-//             alert($("img").attr("data-villian"));
+//             console.log("Name: ", $("#emp").data("name"));
+//             console.log("Year: ",$("#emp").data("year"));
+//             console.log("Year: ",$("#emp").data("year"));
 //         });
-
-//         $("#change").click(function(){
-//             $("img").attr("data-villian","Galactus");
-//             alert($("img").attr("data-villian"));
+//         $("#btn1").click(function(){
+//             console.log("Name: ",$("#emp").html());
 //         });
-
-//         console.log($("#home").html());
-//         console.log($("#home").text());
+//         $("#btn2").click(function(){
+//             console.log("Href: ",$("#w3s").attr("href"));
+//             $("#home").attr("title","New Anchor title");
+//             console.log($("#home").attr("title"));
+//         });
 //     });
+
+// $(document).ready(function(){
+//         $("#btn1").click(function(){
+//             $("#test1").text("I am updated paragraph");
+//         });
+//         $("#btn2").click(function(){
+//             $("#test2").html("<b>I am a bold para</b>")
+//         });
+//         $("#btn3").click(function(){
+//             $("#test3").val("Donald Trump");
+//         });
+//         $("#btn4").click(function(){
+//             console.log($("#test1").text());
+//         });
+//          $("#btn5").click(function(){
+//             console.log($("#test2").html());
+//         });
+//          $("#btn6").click(function(){
+//             console.log($("#test3").val());
+//         });
+//     })
+
+    // $(document).ready(function(){
+    //     $("#btn7").click(function(){
+    //         $("#w3s").attr({
+    //             "href": "https://www.w3schools.com",
+    //             "title": "W3 schools tutorial learning"
+    //         })
+    //     })
+    // })
