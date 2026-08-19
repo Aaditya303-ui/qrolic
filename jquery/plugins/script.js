56d8a9330c504d1d621672a607a1d6306b64155f
Aaditya jadeja <shieldshadow2004@gmail.com>
2026-08-19 18:06:43 +0530

    started with plugins

---
+++ jquery/plugins/script.js
@@ -0,0 +1,9 @@
+// jQuery.fn.makeGreen = function () {
+//   return this.css("color", "green");
+// };
+
+// $.fn.bgBlue = function () {
+//   return this.css("background-color", "aqua");
+// };
+// $("p").makeGreen();
+// $("#box").bgBlue();
