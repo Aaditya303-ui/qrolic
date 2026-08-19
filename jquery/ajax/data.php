5e71b032c1aeb38fe878eb5627c084050f57ac82
Aaditya jadeja <shieldshadow2004@gmail.com>
2026-08-19 16:41:17 +0530

    completed ajax in jquery

---
+++ jquery/ajax/data.php
@@ -0,0 +1,13 @@
+<?php
+
+// $name = $_POST['name'];
+// $age = $_POST['age'];
+
+// $name = $_GET['name'];
+// $age = $_GET['age'];
+
+// echo json_encode([
+//     "message" => "Hello " . $name,
+//     "age" => "Age " . $age
+// ]);
+
---
+++ jquery/ajax/demo.php
@@ -0,0 +1,38 @@
+<?php
+
+// $username = $_GET['username'];
+
+// echo "You searched for: " . $username;
+
+
+// $name = $_POST['email'];
+// $age = $_POST['age'];
+
+// $name = $_POST['email'];
+// $password = $_POST['password'];
+
+// if($name == "abc@gmail.com" && $password = 12345){
+//     echo "Welcome $name";
+// }else{
+//     echo "Invalid credentials";
+// }
+
+// echo "Profile updated successfully!<br>";
+// echo "Name: " . $name . "<br>";
+// echo "Age: " . $age;
+
+// $name = $_POST['name'];
+// $age = $_POST['age'];
+
+// echo "Profile Updated: <br> Name: $name<br> age: $age";
+
+// $name = $_POST['name'];
+
+// echo "Hello $name";
+$id = $_GET['id'];
+
+echo json_encode([
+    "id" => $id,
+    "name" => "Laptop",
+    "price" => 55000
+]);
